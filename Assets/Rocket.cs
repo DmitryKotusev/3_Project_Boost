9a96c8a1587145f8fe325c903a245c1d32a34da5
DmitryKotusev <dssa-1966@yandex.ru>
2019-03-18 01:19:14 +0300

    Added flying ability.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,12 +22,12 @@ public class Rocket : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            // up
+            rigidBody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            // left
+            // rigidBody.transform.RotateAroundLocal()
         }
         else if (Input.GetKey(KeyCode.D))
         {
